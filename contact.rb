344f9512e1d23af19941924f2dd576ff8257069d
Bennett Grigull <grigullb@kenyon.edu>
2016-07-05 17:16:03 -0700

    Update contact.rb

--- contact.rb
+++ contact.rb
@@ -14,14 +14,19 @@ class Contact
     @saved = false
   end
 
-  def is_saved?
-    @id != nil
-  end
-
   def set_id
     conn = Contact.connection
     res = conn.exec_params("SELECT id FROM contacts WHERE name LIKE '#{@name}'")
     self.id = res[0]['id']
+  end
+
+  def same_email?(email)
+    check = Contact.search(email)
+    if check.any?
+      true
+    else
+      false
+    end
   end
 
   def save
@@ -54,11 +59,15 @@ class Contact
       conn = self.connection
       res = conn.exec_params('SELECT * FROM contacts')
     end
+
     def create(name,email)
       contact = Contact.new(name, email)
-      contact.save
+      if contact.same_email?(email)
+        puts "A contact already has that email address"
+      else
+        contact.save
+      end
     end
-    
     
     def find(id)
       conn = self.connection
@@ -72,16 +81,14 @@ class Contact
     def search(term)
       found = []
       conn = self.connection
-      res = conn.exec_params("SELECT id, name, email FROM contacts WHERE name LIKE '%#{term}%'"). each do |row|
+      res = conn.exec_params("SELECT id, name, email FROM contacts WHERE name LIKE '%#{term}%' OR email LIKE '%#{term}%'"). each do |row|
         contact = Contact.new(row['name'], row['email'])
         contact.set_id
         found << contact
       end
       found
     end
-
   end
-
 end
 
 
